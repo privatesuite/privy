e0f299e4dac1afde114034408d08d3058d2413b9
SuperAuguste <auguste.rame@gmail.com>
2019-06-06 08:40:51 +0300

    Web Only articles!

---
+++ tools/mk_dist.bat
@@ -0,0 +1,23 @@
+cls
+@ECHO off
+
+echo Starting creation of distribution folder
+
+rmdir "../staging/dist" /s /q > nul
+mkdir "../staging/dist" > nul
+
+mkdir "../staging/dist/js" > nul
+mkdir "../staging/dist/css" > nul
+mkdir "../staging/dist/img" > nul
+mkdir "../staging/dist/views" > nul
+
+copy /Y "../js" "../staging/dist/js" > nul
+copy /Y "../css" "../staging/dist/css" > nul
+copy /Y "../img" "../staging/dist/img" > nul
+copy /Y "../views" "../staging/dist/views" > nul
+
+more "../index.html" > "../staging/dist/index.html"
+
+echo Distribution folder is ready
+
+@ECHO on
